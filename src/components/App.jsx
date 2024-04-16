30ddb0f491b8b2c217998db64c3856122e6dd52c
ErwinAdriel <adrielquispe87@gmail.com>
2024-04-15 21:56:02 -0300

    btnLike y display

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,14 @@ import { useState } from 'react';
 function App() {
 
   const [likes, setLikes] = useState (0);
+  const darLike = () =>{
+    setLikes(likes + 1);
+  };
 
   return (
     <div className='body'>
       <h1>Likes {likes}</h1>
-      <button onClick={setLikes}>Me gusta</button>
+      <button onClick={darLike}>Me gusta</button>
     </div>
   );
 }
